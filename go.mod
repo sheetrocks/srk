76fe2acc19b3fc365df19cb7af00274a85ba95ed
Anthony Garvan <anthony.garvan@gmail.com>
2021-08-14 15:36:03 -0500

    adding spinner

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/sheetrocks/srk
 
 go 1.15
+
+require (
+	github.com/briandowns/spinner v1.16.0 // indirect
+	github.com/inancgumus/screen v0.0.0-20190314163918-06e984b86ed3 // indirect
+	golang.org/x/crypto v0.0.0-20210813211128-0a44fdfbc16e // indirect
+)
--- srk.go
+++ srk.go
@@ -9,6 +9,9 @@ import (
 	"os"
 	"path"
 	"strings"
+	"time"
+
+	"github.com/briandowns/spinner"
 
 	v "github.com/sheetrocks/srk/values"
 )
@@ -150,6 +153,11 @@ func main() {
 		return
 	}
 
+	s := spinner.New(spinner.CharSets[36], 100*time.Millisecond)
+	s.Color("fgBlack")
+	s.FinalMSG = ""
+	s.Start()
+
 	formulaBody := FormulaBody{Name: strings.ToUpper(config.Name), Runtime: config.Runtime, Help: string(helpText), ScriptText: string(scriptText), Dependencies: string(dependenciesText)}
 
 	body, _ := json.Marshal(formulaBody)
@@ -167,186 +175,11 @@ func main() {
 		panic(err)
 	}
 
+	s.Stop()
 	if resp.StatusCode == 200 {
 		fmt.Printf("🎉 Success! You have pushed your formula \"%s\" to SheetRocks 🎉\n", formulaBody.Name)
 	} else {
 		bytes, _ := ioutil.ReadAll(resp.Body)
 		fmt.Println("SheetRocks error: ", string(bytes))
 	}
-	/*
-		if info.IsDir() {
-			files, _ := ioutil.ReadDir(filepath)
-
-			chartSubmission := ChartTypeSubmission{}
-			chartSubmission.Assets = []Asset{}
-			hasIndex := false
-			hasHelp := false
-			hasConfig := false
-
-			_, f := path.Split(filepath)
-			chartSubmission.Title = f
-			for _, file := range files {
-				if file.Name() == "index.html" {
-					hasIndex = true
-				}
-				switch file.Name() {
-				case "parameters.json":
-					parameterPath := path.Join(filepath, file.Name())
-					dat, err := ioutil.ReadFile(parameterPath)
-					var parametersConfig ParametersConfig
-					err = json.Unmarshal(dat, &parametersConfig)
-
-					if err != nil {
-						fmt.Println("Error encountered while trying to read parameters.json file")
-						fmt.Println(err)
-						return
-					}
-					chartSubmission.Parameters = parametersConfig.Parameters
-					hasConfig = true
-					break
-				case "help.md":
-					dat, _ := ioutil.ReadFile(path.Join(filepath, file.Name()))
-					chartSubmission.Help = string(dat)
-					hasHelp = true
-					break
-				default:
-					dat, _ := ioutil.ReadFile(path.Join(filepath, file.Name()))
-					mimeType := ""
-
-					switch path.Ext(file.Name()) {
-					case ".html":
-						mimeType = "text/html"
-						break
-					case ".js":
-						mimeType = "application/javascript"
-						break
-					case ".css":
-						mimeType = "text/plain"
-						break
-					}
-
-					if mimeType != "" {
-						chartSubmission.Assets = append(chartSubmission.Assets, Asset{file.Name(), mimeType, string(dat)})
-					}
-
-				}
-			}
-
-			if !hasIndex {
-				fmt.Println("Missing required file: index.html")
-				return
-			}
-
-			if !hasConfig {
-				fmt.Println("Missing required file: parameters.json")
-				return
-			}
-
-			if !hasHelp {
-				fmt.Println("Missing required file: help.md")
-				return
-			}
-
-
-			req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/chart-type", baseUrl), nil)
-
-			req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
-			req.Header.Add("Content-Type", "application/json")
-
-			client := &http.Client{}
-			resp, err := client.Do(req)
-			if err != nil {
-				panic(err)
-			}
-
-			if resp.StatusCode == 200 {
-				chartTypes := []ChartType{}
-				json.NewDecoder(resp.Body).Decode(&chartTypes)
-				resp.Body.Close()
-
-				body, _ := json.Marshal(chartSubmission)
-				r := bytes.NewReader(body)
-				foundChartID := ""
-
-				for _, ct := range chartTypes {
-					if ct.Title == chartSubmission.Title {
-						foundChartID = ct.ID
-					}
-				}
-
-				var outputType string
-				if foundChartID == "" {
-					outputType = "added"
-					req, err = http.NewRequest(http.MethodPost, fmt.Sprintf("%s/chart-type", baseUrl), r)
-				} else {
-					outputType = "updated"
-					req, err = http.NewRequest(http.MethodPut, fmt.Sprintf("%s/chart-type/%s", baseUrl, foundChartID), r)
-				}
-
-				req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
-				req.Header.Add("Content-Type", "application/json")
-
-				client := &http.Client{}
-				resp, err := client.Do(req)
-				if err != nil {
-					panic(err)
-				}
-
-				if resp.StatusCode == 200 {
-					fmt.Printf("🎉 Successfully %s new chart \"%s\" 🎉\n", outputType, chartSubmission.Title)
-				} else {
-					fmt.Printf("Encountered unexpected status code: %d\n", resp.StatusCode)
-				}
-			}
-		} else {
-			dat, err := ioutil.ReadFile(filepath)
-
-			if err != nil {
-				fmt.Println("Error: Formula file was not found.")
-				fmt.Println(err)
-				return
-			}
-
-			scriptText := string(dat)
-
-			dir, filename := path.Split(filepath)
-
-			formulaName := strings.Split(filename, ".")[0]
-			markdownPath := path.Join(dir, fmt.Sprintf("%s.md", formulaName))
-
-			dat, err = ioutil.ReadFile(markdownPath)
-
-			if err != nil {
-				fmt.Println("Error: Help document is required.")
-				fmt.Println(err)
-				return
-			}
-
-			help := string(dat)
-
-			formulaBody := FormulaBody{Name: strings.ToUpper(formulaName), Help: help, ScriptText: scriptText}
-
-			body, _ := json.Marshal(formulaBody)
-
-			r := bytes.NewReader(body)
-
-			req, err := http.NewRequest(http.MethodPut, fmt.Sprintf("%s/formula", baseUrl), r)
-
-			req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
-			req.Header.Add("Content-Type", "application/json")
-
-			client := &http.Client{}
-			resp, err := client.Do(req)
-			if err != nil {
-				panic(err)
-			}
-
-			if resp.StatusCode == 200 {
-				fmt.Printf("🎉 Success! You have pushed your formula \"%s\" to SheetRocks 🎉\n", formulaBody.Name)
-			} else {
-				bytes, _ := ioutil.ReadAll(resp.Body)
-				fmt.Println("SheetRocks error: ", string(bytes))
-			}
-		}
-	*/
 }
