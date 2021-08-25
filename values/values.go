6f14b0f48454f0e7e77433f94e1ece9f5acd9878
Anthony Garvan <anthony.garvan@gmail.com>
2021-08-25 05:58:03 -0500

    adding String() interface to Value

--- values/values.go
+++ values/values.go
@@ -1,6 +1,9 @@
 package values
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type ValueType uint8
 
@@ -21,4 +24,28 @@ type Value struct {
 	Date    time.Time
 	Boolean bool
 	Array   [][]Value
+}
+
+func (v *Value) String() string {
+	switch v.Type {
+	case EMPTY:
+		return ""
+	case NUMBER:
+		return fmt.Sprintf("%f", v.Number)
+	case DATE:
+		return v.Date.Format(time.RFC3339)
+	case BOOLEAN:
+		if v.Boolean {
+			return "TRUE"
+		} else {
+			return "FALSE"
+		}
+	case TEXT:
+		return v.Text
+	case ERROR:
+		return v.Text
+	default:
+		// don't handle arrays for now
+		return ""
+	}
 }
